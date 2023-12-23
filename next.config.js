af792dc92da3cb7b8b3b6cb765661fcc53bf806d
Inderjot Singh <inderjotsingh141@gmail.com>
2023-12-23 18:47:25 +0530

    docker file done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import('next').NextConfig} 
+*/
+const nextConfig = {
+   output: 'standalone'
+}
 
 module.exports = nextConfig
